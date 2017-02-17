8d5ea4890adc6148cfea7ae0101be67d6ee95433
Claire <Claire@claire-lt>
2017-02-17 17:52:24 -0500

    Pushing last changes

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalog.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalog.java
@@ -56,7 +56,7 @@ public class CourseCatalog {
 	 * @return whether the course can be added or not
 	 */
 	public boolean addCourseToCatalog(String name, String title, String section, int credits, String instructorId, String meetingDays, int startTime, int endTime){
-		Course c = null;
+		Course c = null; 
 		try{
 			if(meetingDays.equals("A")){
 				c = new Course(name, title, section, credits, instructorId, meetingDays);
