5c88344ae55568c4af66cee3a8898705a5e8bdcf
Claire <Claire@claire-lt>
2017-02-21 11:35:39 -0500

    Created more tests for CourseNameValidatorFSM - pair programming

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/course/validator/CourseNameValidatorFSMTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/course/validator/CourseNameValidatorFSMTest.java
@@ -8,7 +8,43 @@ public class CourseNameValidatorFSMTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		CourseNameValidatorFSM fsm = new CourseNameValidatorFSM();
+		/*test valid input*/
+		try{
+			assert(fsm.isValid("CSC216"));
+		}
+		catch(InvalidTransitionException e)
+		{
+			fail();
+		}
+		/*test start with number*/
+		try{
+			assert(fsm.isValid("216CSC"));
+			fail();
+		}
+		catch(InvalidTransitionException e)
+		{
+			//skip
+		}
+		/*test start with more than four letters*/
+		try{
+			assert(fsm.isValid("Claire216"));
+			fail();
+		}
+		catch(InvalidTransitionException e)
+		{
+			//skip
+		}
+		/*test end with multiple letters*/
+		try{
+			assert(fsm.isValid("CSC216Awesome"));
+			fail();
+		}
+		catch(InvalidTransitionException e)
+		{
+			//skip
+		}
+		
 	}
 
 }
