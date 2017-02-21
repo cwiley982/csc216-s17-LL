864ca51db8d40f508221a07aac8f4f9179047656
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-02-21 11:27:25 -0500

    Added CourseNameValidatorFSM code and created a test class
    pair programmed

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/validator/InvalidTransitionException.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/validator/InvalidTransitionException.java
@@ -7,31 +7,32 @@ package edu.ncsu.csc216.pack_scheduler.course.validator;
  * @author Caitlyn
  * @author Claire
  */
-public class InvalidTransitionException extends Exception{
-	/**The message that displays during a transition exception**/
+public class InvalidTransitionException extends Exception {
+	/** The message that displays during a transition exception **/
 	private String message;
 	/** ID used for serialization. */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * Creates a new transition exception with a custom message
-	 * @param message the custom exception message.
+	 * 
+	 * @param message
+	 *            the custom exception message.
 	 */
-	public InvalidTransitionException(String message)
-	{
+	public InvalidTransitionException(String message) {
 		this.message = message;
 	}
+
 	/**
-	 * Creates a new transition exception with the default exception
-	 * message.
+	 * Creates a new transition exception with the default exception message.
 	 */
-	public InvalidTransitionException()
-	{
+	public InvalidTransitionException() {
 		this("Invalid FSM Transition.");
 	}
 
 	/**
 	 * Returns the transition conflict message
+	 * 
 	 * @return the message
 	 */
 	public String getMessage() {
---
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/course/validator/CourseNameValidatorFSMTest.java
@@ -0,0 +1,14 @@
+package edu.ncsu.csc216.pack_scheduler.course.validator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CourseNameValidatorFSMTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
