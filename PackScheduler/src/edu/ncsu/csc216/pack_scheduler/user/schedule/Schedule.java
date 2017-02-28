aa8a7e89eb487069cc4868010df02761586eeeb3
Sam Weninger <smwening@ncsu.edu>
2017-02-28 11:56:39 -0500

    Replaced the thrown conflict exception with an IlegalArgumentException.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/schedule/Schedule.java
@@ -26,9 +26,8 @@ public class Schedule {
 	 * @param c the course to add
 	 * @return true if it is added and false if it is not
 	 * @throws IllegalArgumentException if the course that wants to be added is a duplicate
-	 * @throws ConflictException if the course that wants to be added has a conflict with existing courses
 	 */
-	public boolean addCourseToSchedule(Course c) throws IllegalArgumentException, ConflictException {
+	public boolean addCourseToSchedule(Course c) throws IllegalArgumentException {
 		for(int i = 0; i < schedule.size(); i++) {
 			if(c.equals(schedule.get(i))) {
 				throw new IllegalArgumentException("You are already enrolled in " + schedule.get(i).getName() + ".");
@@ -37,10 +36,10 @@ public class Schedule {
 				c.checkConflict(schedule.get(i));
 			}
 			catch(ConflictException e) {
-				throw new ConflictException("The course cannot be added due to a conflict.");
+				throw new IllegalArgumentException("The course cannot be added due to a conflict.");
 			}
 		}
-		schedule.add(schedule.size(),c);
+		schedule.add(schedule.size(), c);
 		return true;
 	}
 	
@@ -95,7 +94,7 @@ public class Schedule {
 	/**
 	 * Sets the schedule's title
 	 * @param s the title to set
-	 * @throws IllegalArgumentException if the paramater is null
+	 * @throws IllegalArgumentException if the parameter is null
 	 */
 	public void setTitle(String s) throws IllegalArgumentException {
 		if(s == null) {
