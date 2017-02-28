91c32dd4f971c81345cdf505b9079aeeee8cf2d1
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-02-28 12:04:32 -0500

    Finished testing schedule
    pair programmed

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/schedule/ScheduleTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/user/schedule/ScheduleTest.java
@@ -1,6 +1,7 @@
 package edu.ncsu.csc216.pack_scheduler.user.schedule;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -56,21 +57,63 @@ public class ScheduleTest {
 
 	@Test
 	public void testRemoveCourseFromSchedule() {
-		fail();
+		Schedule s = new Schedule();
+		assertEquals(s.getTitle(), "My Schedule");
+		Course course = new Course("CSC216", "Intro to Programming - Java", "001", 3, "cjwiley2", "MWF", 1230, 1330);
+		Course course2 = new Course("CSC226", "Discrete Math", "002", 3, "cjwiley2", "MF", 1300, 1400);
+		try {
+			assertTrue(s.addCourseToSchedule(course));
+		} catch (IllegalArgumentException e) {
+			fail();
+		} catch (ConflictException e) {
+			fail();
+		}
+
+		assertTrue(s.removeCourseFromSchedule(course));
+		assertFalse(s.removeCourseFromSchedule(course2));
 	}
 
 	@Test
 	public void testResetSchedule() {
-		fail();
+		Schedule s = new Schedule();
+		assertEquals(s.getTitle(), "My Schedule");
+		try {
+			Course course = new Course("CSC216", "Intro to Programming - Java", "001", 3, "cjwiley2", "MWF", 1230,
+					1330);
+			assertTrue(s.addCourseToSchedule(course));
+		} catch (IllegalArgumentException e) {
+			fail();
+		} catch (ConflictException e) {
+			fail();
+		}
+		s.resetSchedule();
+		assertEquals(0, s.getScheduledCourses().length);
 	}
 
 	@Test
 	public void testGetScheduledCourses() {
-		fail();
+		Schedule s = new Schedule();
+		Course course = new Course("CSC216", "Intro to Programming - Java", "001", 3, "cjwiley2", "MWF", 1230, 1330);
+		Course course2 = new Course("CSC226", "Discrete Math", "002", 3, "cjwiley2", "MF", 1400, 1500);
+		try {
+			assertTrue(s.addCourseToSchedule(course));
+			assertTrue(s.addCourseToSchedule(course2));
+		} catch (IllegalArgumentException e) {
+			fail();
+		} catch (ConflictException e) {
+			fail();
+		}
+		assertEquals("Discrete Math", s.getScheduledCourses()[1][2]);
 	}
 
 	@Test
 	public void testSetTitle() {
-		fail();
+		Schedule s = new Schedule();
+		try {
+			s.setTitle(null);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals("Title cannot be null.", e.getMessage());
+		}
 	}
 }
