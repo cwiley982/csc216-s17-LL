0170ac070a5c11131dea3b38d7b0242c23d60814
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-02-28 11:19:38 -0500

    Added add method and ensureCapacity to ArrayList
    pair programmed

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -40,7 +40,7 @@ public class ArrayList<E> {
 	 */
 	public Object get(int index) {
 		if (size != 0) {
-			return list[index];
+			return (E) list[index];
 		} else {
 			return null;
 		}
@@ -53,6 +53,66 @@ public class ArrayList<E> {
 	 */
 	public int size() {
 		return size;
+	}
+
+	/**
+	 * Adds and element to a specific index and shifts all other elements to
+	 * make room
+	 * 
+	 * @param index
+	 *            the index to add the element to
+	 * @param element
+	 *            the element to be added to the list
+	 */
+	public void add(int index, E element) {
+		if (element == null) {
+			throw new NullPointerException();
+		} else if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		for (int i = 0; i < size; i++) {
+			if (element.equals(list[i])) {
+				throw new IllegalArgumentException();
+			}
+		}
+
+		ensureCapacity(size + 1);
+		for (int i = size; i > index; i--) {
+			list[i] = list[i - 1];
+		}
+		list[index] = element;
+		size++;
+	}
+
+	public void remove(int index) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		for (int i = )
+	}
+
+	/**
+	 * Ensures that the list has enough room to add the element, if not a new,
+	 * longer list is made
+	 * 
+	 * @param capacity
+	 *            the new size of the list once the element is added
+	 */
+	@SuppressWarnings("unchecked")
+	private void ensureCapacity(int capacity) {
+		if (capacity > list.length) {
+			int newCapacity = capacity * 2;
+			if (capacity > newCapacity) {
+				newCapacity = capacity;
+			}
+			E[] newList = (E[]) (new Object[newCapacity]);
+			for (int i = 0; i < size; i++) {
+				newList[i] = list[i];
+			}
+			list = newList;
+		}
+
 	}
 
 }
