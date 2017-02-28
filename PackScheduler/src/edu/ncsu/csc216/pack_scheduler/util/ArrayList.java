5eb22d68ec28def309b787bcca90e8a5ae0dbee2
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-02-28 10:57:04 -0500

    Implemented and javadoc-ed ArrayList class
    pair programmed

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -3,17 +3,27 @@
  */
 package edu.ncsu.csc216.pack_scheduler.util;
 
-import java.util.AbstractList;
-
 /**
+ * Creates a generic ArrayList constructor
+ * 
  * @author Sam
+ * @author Claire
+ * @author Caitlyn
  *
+ * @param <E>
+ *            a generic object
  */
 public class ArrayList<E> {
-	private int INIT_SIZE = 10;
+	/** The initial size of the arraylist */
+	private static final int INIT_SIZE = 10;
+	/** The arraylist that will be used */
 	private E[] list;
+	/** The size of the arraylist */
 	private int size;
 	
+	/**
+	 * Contructs a generic arraylist
+	 */
 	@SuppressWarnings("unchecked")
 	public ArrayList() {
 		E item = (E)(new Object());
@@ -21,14 +31,28 @@ public class ArrayList<E> {
 		size = 0;
 	}
 	
+	/**
+	 * Returns the object in the list at the specified index
+	 * 
+	 * @param index
+	 *            the index to return an object from
+	 * @return the object at the specified index
+	 */
 	public Object get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		if (size != 0) {
+			return list[index];
+		} else {
+			return null;
+		}
 	}
 
+	/**
+	 * Returns the number of elements in the list
+	 * 
+	 * @return the size of the list
+	 */
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 }
