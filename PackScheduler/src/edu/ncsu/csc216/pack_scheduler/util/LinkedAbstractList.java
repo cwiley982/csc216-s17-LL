c0534e4fea0266df0887437162b6293d92a2a957
Claire <Claire@claire-lt>
2017-03-14 10:49:42 -0400

    Started LinkedAbstractList and its test - pair programming

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
@@ -0,0 +1,31 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import java.util.AbstractList;
+
+public class LinkedAbstractList<E> extends AbstractList<E>{
+
+	@Override
+	public Object get(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	private class ListNode {
+		private E data;
+		private ListNode next;
+		
+		public ListNode(E data){
+			
+		}
+		public ListNode(E data, ListNode next){
+			
+		}
+	 }
+	
+}
