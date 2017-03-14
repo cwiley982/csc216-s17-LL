8edc9f0942eecf2edb654aac8ed3db372a4b1b72
Caitlyn <Caitlyn@Caitlyns-Laptop>
2017-03-14 11:23:46 -0400

    Added add method and size method, put stuff in Node constructors
    pair programmed

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
@@ -2,29 +2,89 @@ package edu.ncsu.csc216.pack_scheduler.util;
 
 import java.util.AbstractList;
 
-public class LinkedAbstractList<E> extends AbstractList<E>{
+public class LinkedAbstractList<E> extends AbstractList<E> {
 
+	private int size;
+	private int capacity;
+	private ListNode front;
+
+	public void LinkedAbstractList(int capacity) {
+		size = 0;
+		if (capacity > 0) {
+			this.capacity = capacity;
+		} else {
+			throw new IllegalArgumentException();
+		}
+		front = null;
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
-	public Object get(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public E get(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		ListNode current = front;
+		for (int i = 0; i < index; i++) {
+			current = current.next;
+		}
+		return (E) current.next.data;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 	
+	@Override
+	public void add(int index, E element) {
+		if (element == null) {
+			throw new NullPointerException();
+		} else if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+
+		if (index == 0) {
+			if (!element.equals(front)) {
+				front = new ListNode(element);
+			}
+		} else if (index == size) {
+			ListNode current = front;
+			for (int i = 0; i < size - 1; i++) {
+				current = current.next;
+				if (current.equals(element)) {
+					throw new IllegalArgumentException();
+				}
+			}
+			current.next = new ListNode(element, null);
+		} else {
+			ListNode current = front;
+			ListNode indexToAddAt = null;
+			for (int i = 0; i < size; i++) {
+				current = current.next;
+				if (i == index - 1) {
+					indexToAddAt = current;
+				}
+				if (current.equals(element)) {
+					throw new IllegalArgumentException();
+				}
+			}
+			indexToAddAt = new ListNode(element, indexToAddAt.next);
+		}
+		size++;
+	}
+
 	private class ListNode {
 		private E data;
 		private ListNode next;
 		
 		public ListNode(E data){
-			
+			this.data = data;
 		}
+
 		public ListNode(E data, ListNode next){
-			
+			this.data = data;
+			this.next = next;
 		}
 	 }
 	
